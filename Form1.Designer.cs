f97dba02a1d01d16cf575b3635876a0e5a2be8cd
Mendozalz <mendoza124302@gmail.com>
2024-02-09 15:16:15 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace MyBrowser_Mendozalz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picBack = new System.Windows.Forms.PictureBox();
+            this.picNext = new System.Windows.Forms.PictureBox();
+            this.picUpdate = new System.Windows.Forms.PictureBox();
+            this.picHome = new System.Windows.Forms.PictureBox();
+            this.picStop = new System.Windows.Forms.PictureBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.picGo = new System.Windows.Forms.PictureBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNext)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUpdate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBack
+            // 
+            this.picBack.Image = ((System.Drawing.Image)(resources.GetObject("picBack.Image")));
+            this.picBack.Location = new System.Drawing.Point(12, 12);
+            this.picBack.Name = "picBack";
+            this.picBack.Size = new System.Drawing.Size(24, 22);
+            this.picBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBack.TabIndex = 1;
+            this.picBack.TabStop = false;
+            this.picBack.Click += new System.EventHandler(this.picBack_Click);
+            // 
+            // picNext
+            // 
+            this.picNext.Image = ((System.Drawing.Image)(resources.GetObject("picNext.Image")));
+            this.picNext.Location = new System.Drawing.Point(52, 12);
+            this.picNext.Name = "picNext";
+            this.picNext.Padding = new System.Windows.Forms.Padding(5);
+            this.picNext.Size = new System.Drawing.Size(24, 22);
+            this.picNext.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picNext.TabIndex = 1;
+            this.picNext.TabStop = false;
+            this.picNext.Click += new System.EventHandler(this.picFoward_Click);
+            // 
+            // picUpdate
+            // 
+            this.picUpdate.Image = ((System.Drawing.Image)(resources.GetObject("picUpdate.Image")));
+            this.picUpdate.Location = new System.Drawing.Point(91, 12);
+            this.picUpdate.Name = "picUpdate";
+            this.picUpdate.Padding = new System.Windows.Forms.Padding(3);
+            this.picUpdate.Size = new System.Drawing.Size(24, 22);
+            this.picUpdate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picUpdate.TabIndex = 1;
+            this.picUpdate.TabStop = false;
+            this.picUpdate.Click += new System.EventHandler(this.picReload_Click);
+            // 
+            // picHome
+            // 
+            this.picHome.Image = ((System.Drawing.Image)(resources.GetObject("picHome.Image")));
+            this.picHome.Location = new System.Drawing.Point(131, 12);
+            this.picHome.Name = "picHome";
+            this.picHome.Size = new System.Drawing.Size(24, 22);
+            this.picHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picHome.TabIndex = 1;
+            this.picHome.TabStop = false;
+            this.picHome.Click += new System.EventHandler(this.picHome_Click);
+            // 
+            // picStop
+            // 
+            this.picStop.Image = ((System.Drawing.Image)(resources.GetObject("picStop.Image")));
+            this.picStop.Location = new System.Drawing.Point(172, 12);
+            this.picStop.Name = "picStop";
+            this.picStop.Size = new System.Drawing.Size(24, 22);
+            this.picStop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picStop.TabIndex = 1;
+            this.picStop.TabStop = false;
+            this.picStop.Click += new System.EventHandler(this.picStop_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(216, 14);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(344, 20);
+            this.txtUrl.TabIndex = 2;
+            // 
+            // picGo
+            // 
+            this.picGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picGo.Image = ((System.Drawing.Image)(resources.GetObject("picGo.Image")));
+            this.picGo.Location = new System.Drawing.Point(578, 12);
+            this.picGo.Name = "picGo";
+            this.picGo.Size = new System.Drawing.Size(24, 22);
+            this.picGo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGo.TabIndex = 1;
+            this.picGo.TabStop = false;
+            this.picGo.Click += new System.EventHandler(this.picGo_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 51);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(590, 398);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 461);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.picNext);
+            this.Controls.Add(this.picUpdate);
+            this.Controls.Add(this.picGo);
+            this.Controls.Add(this.picStop);
+            this.Controls.Add(this.picHome);
+            this.Controls.Add(this.picBack);
+            this.MinimumSize = new System.Drawing.Size(630, 500);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNext)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUpdate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBack;
+        private System.Windows.Forms.PictureBox picNext;
+        private System.Windows.Forms.PictureBox picUpdate;
+        private System.Windows.Forms.PictureBox picHome;
+        private System.Windows.Forms.PictureBox picStop;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.PictureBox picGo;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyBrowser_Mendozalz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void picBack_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void picGo_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(txtUrl.Text);
+
+        }
+
+        private void picFoward_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void picStop_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Stop();
+        }
+
+        private void picHome_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void picReload_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+    }
+}
